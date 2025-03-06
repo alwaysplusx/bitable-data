5fac4ccc1ed5e57c80fa14a6e05f928a49d2edc1
wuxin <alwaysplusx@gmail.com>
2025-03-06 22:34:14 +0800

    adapt InvocationTargetException

--- src/main/kotlin/com/harmony/bitable/oapi/ratelimiter/RateLimiterInvocationHandler.kt
+++ src/main/kotlin/com/harmony/bitable/oapi/ratelimiter/RateLimiterInvocationHandler.kt
@@ -5,6 +5,7 @@ import io.github.resilience4j.ratelimiter.RateLimiterRegistry
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter
 import org.springframework.core.annotation.AnnotationUtils
 import java.lang.reflect.InvocationHandler
+import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 
 /**
@@ -15,19 +16,29 @@ class RateLimiterInvocationHandler(
     private val rateLimiterRegistry: RateLimiterRegistry
 ) : InvocationHandler {
 
-    override fun invoke(proxy: Any?, method: Method, args: Array<out Any>?): Any {
+    override fun invoke(proxy: Any?, method: Method, args: Array<out Any>?): Any? {
         return doInvoke(proxy, method, args ?: emptyArray())
     }
 
-    private fun doInvoke(proxy: Any?, method: Method, args: Array<out Any>): Any {
+    private fun doInvoke(proxy: Any?, method: Method, args: Array<out Any>): Any? {
         val annotation = getRateLimiterAnnotation(method) ?: return method.invoke(target, *args)
         val hasRateLimiter = rateLimiterRegistry.allRateLimiters.any { it.name == annotation.name }
         if (!hasRateLimiter) {
-            return method.invoke(target, *args)
+            return invokeMethod(method, args)
         }
         val rateLimiter = rateLimiterRegistry.rateLimiter(annotation.name)
         return rateLimiter.executeCheckedSupplier {
-            method.invoke(target, *args)
+            invokeMethod(method, args)
+        }
+    }
+
+    private fun invokeMethod(method: Method, args: Array<out Any>): Any? {
+        try {
+            return method.invoke(target, *args)
+        } catch (e: InvocationTargetException) {
+            throw e.targetException ?: e
+        } catch (e: Exception) {
+            throw e
         }
     }
 
